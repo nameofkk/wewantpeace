d0b4dbd6d174f0567adfa1d33a91c4636b7ca0db
nameofkk <krshin7@gmail.com>
2026-03-17 02:15:04 +0900

    Revert "fix: TWA → WebView 전환 - 태스크 스위처에서 "WeWantPeace" 표시"
    This reverts commit 315b233b2c74ad81be4a393704e4c85cf88aa56a.

--- android-twa-legacy/app/src/main/java/com/wewantpeace/app/LauncherActivity.java
+++ android-twa-legacy/app/src/main/java/com/wewantpeace/app/LauncherActivity.java
@@ -1,169 +1,54 @@
+/*
+ * Copyright 2020 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.wewantpeace.app;
 
-import android.app.Activity;
-import android.app.ActivityManager;
 import android.content.pm.ActivityInfo;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.Color;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
-import android.view.View;
-import android.view.Window;
-import android.view.WindowManager;
-import android.webkit.CookieManager;
-import android.webkit.WebChromeClient;
-import android.webkit.WebResourceRequest;
-import android.webkit.WebSettings;
-import android.webkit.WebView;
-import android.webkit.WebViewClient;
-import android.webkit.ValueCallback;
-import android.content.Intent;
 
-public class LauncherActivity extends Activity {
 
-    private static final String URL = "https://www.wewantpeace.live/";
-    private WebView webView;
-    private ValueCallback<Uri[]> fileUploadCallback;
+
+public class LauncherActivity
+        extends com.google.androidbrowserhelper.trusted.LauncherActivity {
+    
+
+    
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        // Fullscreen immersive mode
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        getWindow().setFlags(
-                WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
-            getWindow().getAttributes().layoutInDisplayCutoutMode =
-                    WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES;
-        }
-
-        // Status bar & navigation bar colors
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            getWindow().setStatusBarColor(Color.parseColor("#0F1729"));
-            getWindow().setNavigationBarColor(Color.parseColor("#0F1729"));
-        }
-
-        // Orientation
+        // Setting an orientation crashes the app due to the transparent background on Android 8.0
+        // Oreo and below. We only set the orientation on Oreo and above. This only affects the
+        // splash screen and Chrome will still respect the orientation.
+        // See https://github.com/GoogleChromeLabs/bubblewrap/issues/496 for details.
         if (Build.VERSION.SDK_INT > Build.VERSION_CODES.O) {
             setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_USER_PORTRAIT);
-        }
-
-        // Task description for Recent Apps
-        int primaryColor = Color.parseColor("#0F1729");
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
-            setTaskDescription(new ActivityManager.TaskDescription(
-                    "WeWantPeace", R.mipmap.ic_launcher, primaryColor));
         } else {
-            Bitmap icon = BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher);
-            setTaskDescription(new ActivityManager.TaskDescription(
-                    "WeWantPeace", icon, primaryColor));
-        }
-
-        // WebView setup
-        webView = new WebView(this);
-        setContentView(webView);
-
-        WebSettings settings = webView.getSettings();
-        settings.setJavaScriptEnabled(true);
-        settings.setDomStorageEnabled(true);
-        settings.setDatabaseEnabled(true);
-        settings.setMediaPlaybackRequiresUserGesture(false);
-        settings.setCacheMode(WebSettings.LOAD_DEFAULT);
-        settings.setMixedContentMode(WebSettings.MIXED_CONTENT_COMPATIBILITY_MODE);
-        settings.setUserAgentString(settings.getUserAgentString() + " WeWantPeaceApp/1.0");
-
-        // Enable cookies
-        CookieManager.getInstance().setAcceptCookie(true);
-        CookieManager.getInstance().setAcceptThirdPartyCookies(webView, true);
-
-        webView.setWebViewClient(new WebViewClient() {
-            @Override
-            public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
-                String url = request.getUrl().toString();
-                // Keep wewantpeace.live URLs in WebView
-                if (url.contains("wewantpeace.live")) {
-                    return false;
-                }
-                // Open external URLs in browser
-                Intent intent = new Intent(Intent.ACTION_VIEW, request.getUrl());
-                startActivity(intent);
-                return true;
-            }
-        });
-
-        webView.setWebChromeClient(new WebChromeClient() {
-            @Override
-            public boolean onShowFileChooser(WebView webView,
-                    ValueCallback<Uri[]> filePathCallback,
-                    FileChooserParams fileChooserParams) {
-                fileUploadCallback = filePathCallback;
-                Intent intent = fileChooserParams.createIntent();
-                startActivityForResult(intent, 100);
-                return true;
-            }
-        });
-
-        // Immersive sticky mode
-        webView.setSystemUiVisibility(
-                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
-
-        // Load the URL
-        String launchUrl = URL;
-        Intent intent = getIntent();
-        if (intent != null && intent.getData() != null) {
-            String data = intent.getData().toString();
-            if (data.contains("wewantpeace.live")) {
-                launchUrl = data;
-            }
-        }
-        webView.loadUrl(launchUrl);
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == 100 && fileUploadCallback != null) {
-            Uri[] results = null;
-            if (resultCode == RESULT_OK && data != null) {
-                results = new Uri[]{data.getData()};
-            }
-            fileUploadCallback.onReceiveValue(results);
-            fileUploadCallback = null;
+            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED);
         }
     }
 
     @Override
-    public void onBackPressed() {
-        if (webView != null && webView.canGoBack()) {
-            webView.goBack();
-        } else {
-            super.onBackPressed();
-        }
-    }
+    protected Uri getLaunchingUrl() {
+        // Get the original launch Url.
+        Uri uri = super.getLaunchingUrl();
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        if (webView != null) webView.onResume();
-    }
+        
 
-    @Override
-    protected void onPause() {
-        if (webView != null) webView.onPause();
-        super.onPause();
-    }
-
-    @Override
-    protected void onDestroy() {
-        if (webView != null) {
-            webView.destroy();
-        }
-        super.onDestroy();
+        return uri;
     }
 }
